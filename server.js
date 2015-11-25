43abe05ae35f714568cab4db61ce4026fff61ee1
Kirill Kozionov <kirill@kozionov.com>
2015-11-25 10:30:24 -0500

    [*] Hello world 1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http = require('http');
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Hello World\n');
+	res.end('Hello World 1\n');
 }).listen(port, function () {
  	SlackService.sendServiceOnlineNotification();
 });
